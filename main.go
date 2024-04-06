5c86eb33577c00be44930e14112937489a6a2c9e
WhiteDeath <shivanshu.tyagi@jukshio.com>
2024-04-06 21:52:16 +0530

    Validate code using this commit

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprint(w, "This line is to validate te code and the test are working properly \n I will commit again")
 }
 
 func main() {
